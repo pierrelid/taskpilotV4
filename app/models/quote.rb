72aabd0229977a236b94f20ebba542656d7b7ad0
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-03 20:21:25 +0200

    s

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,9 @@
 class Quote < ApplicationRecord
+  email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   belongs_to :team
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true, format: { with: email_regex }
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,54 @@
-require 'test_helper'
+require "test_helper"
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid if email is not a good format" do
+    emails = [
+      "email",
+      "email@",
+      "@email",
+      "@email.com",
+      "email@.com",
+    ]
+    emails.each do |email|
+      quote = Quote.new(email: email)
+      quote.valid?
+      assert_not quote.errors[:email].empty?
+    end
+  end
+
+  test "invalid if email is not present" do
+    quote = Quote.new
+    quote.valid?
+    assert_not quote.errors[:email].empty?
+  end
+
+  test "valid if email is present" do
+    quote = Quote.new(email: "email@email.com")
+    quote.valid?
+    assert_empty quote.errors[:email]
+  end
+
+  test "invalid if first_name is not present" do
+    quote = Quote.new
+    quote.valid?
+    assert_not quote.errors[:first_name].empty?
+  end
+
+  test "valid if first_name is present" do
+    quote = Quote.new(first_name: "first_name")
+    quote.valid?
+    assert_empty quote.errors[:first_name]
+  end
+
+  test "invalid if last_name is not present" do
+    quote = Quote.new
+    quote.valid?
+    assert_not quote.errors[:last_name].empty?
+  end
+
+  test "valid if last_name is present" do
+    quote = Quote.new(last_name: "last_name")
+    quote.valid?
+    assert_empty quote.errors[:last_name]
+  end
 end
