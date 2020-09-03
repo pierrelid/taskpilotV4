c50549a316c45247495aff03489f2393e2493a45
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-03 09:25:23 +0200

    logininterface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    # devise 
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+    protected 
+
+    def configure_permitted_parameters
+        # storng devise params
+    end 
 end
