0df1ddd2c3c5b1ec077ee2665aa15ed5335676d3
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-03 13:01:52 +0200

    s

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 if  Rails.env.development?
     # Destroy all database's instances  
     User.destroy_all 
-    
+
     # Instances creation
     admin = User.create(email: "admin@admin.com", password: "123456", admin: true)
-    p "Admin create :#{admin.email}"
+    p "Admin create : #{admin.email}"
 end 
