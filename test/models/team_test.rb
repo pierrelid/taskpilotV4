acf6b7a55c20f73ecde0c8fa1ff149e005f4d0fe
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-06 22:15:41 +0200

    s

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,6 +1,22 @@
 require "test_helper"
 
 class TeamTest < ActiveSupport::TestCase
+  test "list creation after team creation" do
+    team = Team.create(name: "name")
+    names = []
+    Qualification.default_names_for_lists.each do |qualification_name|
+      Product.default_names.each do |product_name|
+        names << "#{product_name} : #{qualification_name}"
+      end
+    end
+    assert_equal ["AUTO : Jamais démarché", "MRH : Jamais démarché", "SANTE : Jamais démarché"].sort, team.lists.map(&:name).sort
+  end
+
+  test "qualification creation after team creation" do
+    team = Team.create(name: "name")
+    assert_equal Qualification.default_names.sort, team.qualifications.map(&:name).sort
+  end
+
   test "product creation after team creation" do
     team = Team.create(name: "name")
     assert_equal ["MRH", "AUTO", "SANTE"].sort, team.products.map(&:name).sort
