c5522ef2c908fdd30cc149bd22d04f787640bca1
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-11 21:30:34 +0200

    add create to quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,26 @@ class QuotesController < ApplicationController
   def index
     @quotes = policy_scope(Quote)
   end
+
+  def new
+    @quote = Quote.new
+    authorize @quote
+  end
+
+  def create
+    @quote = Quote.new(quote_params.merge(team: current_user.team))
+    authorize @quote
+    @quote.save ? redirect_to(quote_path(@quote)) : render(:new)
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+    authorize @quote
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:first_name, :last_name, :email, :phone, :product_id, :qualification_id)
+  end
 end
