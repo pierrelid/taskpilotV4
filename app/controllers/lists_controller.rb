bdf5b99e4e2ee2ef42d4474224e0d04b2f5bf70c
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-12 19:02:23 +0200

    add list policy

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,13 @@ class ListsController < ApplicationController
   def index
     @lists = policy_scope(List)
   end
+
+  def create?
+  end
+
+  def update?
+  end
+
+  def destroy?
+  end
 end
--- app/policies/list_policy.rb
+++ app/policies/list_policy.rb
@@ -4,4 +4,20 @@ class ListPolicy < ApplicationPolicy
       scope.where(team: user.team)
     end
   end
+
+  def create?
+    true
+  end
+
+  def show?
+    record.team = user.team
+  end
+
+  def update?
+    record.team = user.team
+  end
+
+  def destroy?
+    record.team = user.team
+  end
 end
