f231ad17505ad7631c4ed9362c1b63ccaa144d87
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-12 19:12:05 +0200

    crud on list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,48 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
   def index
     @lists = policy_scope(List)
   end
 
-  def create?
+  def show
   end
 
-  def update?
+  def new
+    @list = List.new
+    authorize @list
   end
 
-  def destroy?
+  def create
+    @list = List.new(list_params.merge(team: current_user.team))
+    authorize @list
+    @list.save ? redirect_to(list_path(@list)) : render(:new)
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+    authorize @list
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :qualification_id, :product_id)
   end
 end
