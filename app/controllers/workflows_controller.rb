675be676e01cc4e10f61b31cfa4464284de2641d
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-14 11:24:28 +0200

    add workflow controller methods

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,4 +1,6 @@
 class WorkflowsController < ApplicationController
+  before_action :set_workflow, only: [:show, :edit, :update, :destroy]
+
   def index
     @workflows = policy_scope(Workflow)
   end
@@ -8,9 +10,31 @@ class WorkflowsController < ApplicationController
     authorize @workflow
   end
 
+  def show
+  end
+
+  def create
+    @workflow = Workflow.new(workflow_params)
+    authorize @workflow
+    @workflow.save ? redirect_to(workflow_path(@workflow)) : render(:new)
+  end
+
+  def update
+    if @workflow.update(workflow_params)
+      redirect_to workflow_path(@workflow)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def workflow_params
     params.require(:workflow).permit(:name, :list_id)
+  end
+
+  def set_workflow
+    @workflow = Workflow.find(params[:id])
+    authorize @workflow
   end
 end
