bfd865ebe57b3347e287c837da41d0155a621fbe
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-14 17:22:41 +0200

    add step form

--- app/javascript/packs/controllers/step_controller.js
+++ app/javascript/packs/controllers/step_controller.js
@@ -1,9 +1,40 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["kinfOfStep"];
+  static targets = ["kindOfStep", "titleInput", "bodyInput", "delayInput"];
 
   kind() {
-    console.log("ttt");
+    const kind = this.kindOfStepTarget;
+    const titleInput = this.titleInputTarget;
+    const bodyInput = this.bodyInputTarget;
+    const delayInput = this.delayInputTarget;
+
+    switch (kind.value) {
+      case "EmailStep":
+        this.displayElement(titleInput);
+        this.displayElement(bodyInput);
+        this.hideElement(delayInput);
+        break;
+      case "SmsStep":
+        this.hideElement(titleInput);
+        this.displayElement(bodyInput);
+        this.hideElement(delayInput);
+        break;
+      case "DelayStep":
+        this.hideElement(titleInput);
+        this.hideElement(bodyInput);
+        this.displayElement(delayInput);
+        break;
+    }
+  }
+
+  displayElement(element) {
+    element.classList.remove("d-none");
+    element.classList.add("d-block");
+  }
+
+  hideElement(element) {
+    element.classList.add("d-none");
+    element.classList.remove("d-block");
   }
 }
