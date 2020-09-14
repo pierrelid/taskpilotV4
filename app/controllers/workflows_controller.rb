21e54129e5cefcfd2c84798642e9623e253a9fe7
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-14 11:15:08 +0200

    add workflow params

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -2,4 +2,15 @@ class WorkflowsController < ApplicationController
   def index
     @workflows = policy_scope(Workflow)
   end
+
+  def new
+    @workflow = Workflow.new
+    authorize @workflow
+  end
+
+  private
+
+  def workflow_params
+    params.require(:workflow).permit(:name, :list_id)
+  end
 end
