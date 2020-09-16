923faa63c9011262847d4a84d7e7c1eeb7b285b8
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-16 17:07:43 +0200

    step assignation before destroy

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,10 +1,20 @@
 class Step < ApplicationRecord
   belongs_to :workflow
-  has_many :list_lines
+  has_many :list_lines, dependent: :nullify
 
   acts_as_list scope: :workflow
 
   scope :emails, -> { where(type: "EmailStep") }
   scope :sms, -> { where(type: "SmsStep") }
   scope :delay_steps, -> { where(type: "DelayStep") }
+
+  before_destroy :change_list_lines_step
+
+  def change_list_lines_step
+    self.list_lines.each { |e| e.update(step: next_step) }
+  end
+
+  def next_step
+    self.workflow.steps.find_by(position: self.position + 1)
+  end
 end
--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -1,7 +1,32 @@
-require 'test_helper'
+require "test_helper"
 
 class StepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # change_list_lines_step
+
+  test "change list line step must assign to the next step" do
+    step = steps(:step_1_1_1)
+    list = lists(:list_1_1)
+    quote_1 = quotes(:quote_1_1)
+    quote_2 = quotes(:quote_1_2)
+    list_line_1 = ListLine.create(step: step, quote: quote_1, list: list)
+    list_line_2 = ListLine.create(step: step, quote: quote_2, list: list)
+    step.change_list_lines_step
+    list_line_1.reload
+    list_line_2.reload
+    assert_equal list_line_1.step, steps(:step_1_1_2)
+    assert_equal list_line_2.step, steps(:step_1_1_2)
+  end
+
+  # next_step
+
+  test "next_step must return the next step" do
+    step = steps(:step_1_1_1)
+    assert_equal steps(:step_1_1_2), step.next_step
+  end
+
+  test "next_step must return nil if next_step doesn't exists" do
+    step = steps(:step_1_1_3)
+    assert_nil step.next_step
+  end
 end
