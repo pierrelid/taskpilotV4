1e1ad85da5f76d5b8a71b8114febed72ae19f77b
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-16 11:43:54 +0200

    add mail job

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -1,9 +1,11 @@
 class SendEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(quote_id, user_id)
+  def perform(quote_id, step_id, user_id)
     quote = Quote.find(quote_id)
     user = User.find(user_id)
-    WokflowMailer.personal(quote, user).deliver_later
+    step = Step.find(step_id)
+    email = EmailMessage.create(title: step.title, body: step.body, quote: quote)
+    WokflowMailer.personal(quote, user, email).deliver_later
   end
 end
--- test/jobs/send_email_job_test.rb
+++ test/jobs/send_email_job_test.rb
@@ -1,7 +1,25 @@
-require 'test_helper'
+require "test_helper"
 
 class SendEmailJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @step = steps(:step_1_1_1)
+    @quote = quotes(:quote_1_1)
+  end
+
+  test "must send email with WorkflowMailer service" do
+    perform_enqueued_jobs only: ActionMailer::MailDeliveryJob do
+      SendEmailJob.perform_now(@quote.id, @step.id, @user.id)
+      assert_performed_jobs 1
+    end
+  end
+
+  test "must create a SmsMessage instance" do
+    SendEmailJob.perform_now(@quote.id, @step.id, @user.id)
+    message = Message.last
+    assert_equal "EmailMessage", message.type
+    assert_equal @quote, message.quote
+    assert_equal @step.body, message.body
+    assert_equal @step.title, message.title
+  end
 end
