8d372d99c04965dc95f3ede712c744837f06886e
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-16 14:51:54 +0200

    refacto in model

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -6,4 +6,12 @@ class Workflow < ApplicationRecord
   has_many :list_lines, through: :list
 
   validates :name, presence: true
+
+  def update_waiting_list_lines
+    list_lines = self.list_lines.where(finish: false, waiting: true)
+    list_lines.each do |list_line|
+      update_condition = (DateTime.now.to_i - list_line.waiting_start.to_i) >= list_line.seconds_to_wait
+      list_line.update(waiting: false, seconds_to_wait: 0, waiting_start: nil) if update_condition
+    end
+  end
 end
--- test/models/workflow_test.rb
+++ test/models/workflow_test.rb
@@ -1,6 +1,36 @@
 require "test_helper"
 
 class WorkflowTest < ActiveSupport::TestCase
+
+  #  update_waiting_list_lines
+
+  test "not update list_line if delay is not past" do
+    freeze_time
+    workflow = workflows(:workflow_1_1)
+    list = workflow.list
+    quote = quotes(:quote_1_1)
+    list_line = ListLine.create(quote: quote, list: list, waiting: true, seconds_to_wait: 3600, waiting_start: DateTime.now - 10.minutes)
+    workflow.update_waiting_list_lines
+    list_line.reload
+    assert list_line.waiting
+    assert_equal 3600, list_line.seconds_to_wait
+    assert_equal DateTime.now - 10.minutes, list_line.waiting_start
+  end
+
+  test "update list_line is delay is past" do
+    workflow = workflows(:workflow_1_1)
+    list = workflow.list
+    quote = quotes(:quote_1_1)
+    list_line = ListLine.create(quote: quote, list: list, waiting: true, seconds_to_wait: 3600, waiting_start: DateTime.now - 2.hours)
+    workflow.update_waiting_list_lines
+    list_line.reload
+    assert_not list_line.waiting
+    assert_equal 0, list_line.seconds_to_wait
+    assert_nil list_line.waiting_start
+  end
+
+  # validations
+
   test "valid if list is present" do
     workflow = Workflow.new(list: List.last)
     workflow.valid?
