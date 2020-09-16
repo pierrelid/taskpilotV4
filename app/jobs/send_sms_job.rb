79e02c051b19aacf86739cf9ff78fef8caef2674
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-16 11:15:39 +0200

    "add send sms job tests" " "

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -4,7 +4,7 @@ class SendSmsJob < ApplicationJob
   def perform(quote_id, step_id)
     step = Step.find(step_id)
     quote = Quote.find(quote_id)
-    sms = Message.create(body: step.body, quote: quote)
+    sms = SmsMessage.create(body: step.body, quote: quote)
     InfobipService.new({ message_id: sms.id }).send_sms
   end
 end
