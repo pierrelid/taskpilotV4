850b554524f9b027c280827ca42c874bf0547b4d
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-16 15:55:16 +0200

    add tests on step execution methods

--- app/models/list_line.rb
+++ app/models/list_line.rb
@@ -4,4 +4,18 @@ class ListLine < ApplicationRecord
   belongs_to :step, optional: true
 
   validates :list, uniqueness: { scope: :quote }
+
+  def step_execution
+    if self.step.present?
+      case self.step.type
+      when "EmailStep"
+        SendEmailJob.perform_later(self.quote.id, self.step.id, self.step.workflow.user.id)
+      when "SmsStep"
+        SendSmsJob.perform_later(self.quote.id, self.step.id)
+      when "DelayStep"
+        DelayJob.perform_later(self.step.id, self.id)
+      else
+      end
+    end
+  end
 end
