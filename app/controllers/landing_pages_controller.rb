5cfad1809aed963eea21556fea42e246a8735d6f
Pierre Liduena <pierre@assurancesvelo.com>
2020-09-29 09:48:00 +0200

    add landing page show view

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,7 @@
 class LandingPagesController < ApplicationController
   def show
+    @landing_page = LandingPage.find(params[:id])
+    authorize @landing_page
+    render layout: "landing_page"
   end
 end
