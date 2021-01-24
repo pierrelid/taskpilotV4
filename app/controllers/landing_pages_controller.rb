052c0c7d96ea65abe24dfabf80141a92cd399473
Pierre Liduena <pierre@assurancesvelo.com>
2021-01-24 17:58:55 +0100

    s

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -2,16 +2,8 @@ class LandingPagesController < ApplicationController
   def show
     @landing_page = LandingPage.find_by(slug: params[:slug])
     authorize @landing_page
-    @dates = set_dates
+    @dates = @landing_page.set_dates
+    @hours = @landing_page.set_hours
     render layout: "landing_page"
-  end
-
-  def set_dates
-    dates = []
-    [*0..6].each do |offset|
-      dates << Date.today + offset.day
-      # créer les heures
-    end
-    dates
   end
 end
