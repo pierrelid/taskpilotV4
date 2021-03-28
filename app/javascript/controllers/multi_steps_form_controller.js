94359733a693e9b8a698cbaa0fdbe4ec0ed6532a
Pierre Liduena <pierre@assurancesvelo.com>
2021-03-28 23:21:24 +0200

    s

--- app/javascript/controllers/multi_steps_form_controller.js
+++ app/javascript/controllers/multi_steps_form_controller.js
@@ -11,11 +11,25 @@ export default class extends Controller {
     );
     const nextPosition = currentPosition + 1;
     const steps = document.getElementsByClassName("step");
-    console.log(steps);
-    for (let position = 0; position < steps.length - 1; position++) {
+    for (let position = 0; position < steps.length; position++) {
+      if (position === nextPosition) {
+        steps[position].classList.remove("d-none");
+      } else {
+        steps[position].classList.add("d-none");
+      }
+    }
+  }
+
+  previous(event) {
+    const currentTarget = event.currentTarget;
+    const currentPosition = parseInt(
+      currentTarget.getAttribute("position"),
+      10
+    );
+    const nextPosition = currentPosition - 1;
+    const steps = document.getElementsByClassName("step");
+    for (let position = 0; position < steps.length; position++) {
       console.log(position);
-      console.log(nextPosition);
-      console.log("---");
       if (position === nextPosition) {
         steps[position].classList.remove("d-none");
       } else {
